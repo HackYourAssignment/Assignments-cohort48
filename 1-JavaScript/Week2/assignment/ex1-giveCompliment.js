b9df6aa1cdb550eec23dcb0abb7d72e2318db844
yousraElmag <yousra3489@gmail.com>
2024-04-28 07:26:10 +0200

    first done

--- 1-JavaScript/Week2/assignment/ex1-giveCompliment.js
+++ 1-JavaScript/Week2/assignment/ex1-giveCompliment.js
@@ -18,9 +18,14 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/1-Java
    Use `console.log` each time to display the return value of the
    `giveCompliment` function to the console.
 -----------------------------------------------------------------------------*/
-function giveCompliment(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+function giveCompliment(name) {
+const compliment=['great','smart','honst','quit','beatufal','alegent']
+const randcomp=Math.floor(Math.random() *compliment.length)
+return  `You are ${compliment[randcomp]}, ${name}!, where ${compliment[randcomp]}`
+
+
 }
+giveCompliment()
 
 function main() {
   // TODO substitute your own name for "HackYourFuture"
