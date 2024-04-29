931ebc3d5780752f3e7f2abb2be97ee5d14a4ab0
Alishka <ali.ibrahim.work@gmail.com>
2024-04-29 21:37:56 +0200

    the compliments fixed

--- 1-JavaScript/Week2/assignment/ex1-giveCompliment.js
+++ 1-JavaScript/Week2/assignment/ex1-giveCompliment.js
@@ -18,13 +18,28 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/1-Java
    Use `console.log` each time to display the return value of the
    `giveCompliment` function to the console.
 -----------------------------------------------------------------------------*/
-function giveCompliment(/* TODO parameter(s) go here */) {
+function giveCompliment(name) {
   // TODO complete this function
+  let compliments = [
+    'amazing',
+    'great',
+    'fantastic',
+    'terrific',
+    'incredible',
+    'supreme',
+    'brilliant',
+    'wonderful',
+    'excellent',
+    'outstanding'
+  ];
+  let random = Math.floor(Math.random() * compliments.length);
+  let compliment = compliments[random];
+  return `You are ${compliment} , ${name} !`;
 }
 
 function main() {
   // TODO substitute your own name for "HackYourFuture"
-  const myName = 'HackYourFuture';
+  const myName = 'Ali';
 
   console.log(giveCompliment(myName));
   console.log(giveCompliment(myName));
