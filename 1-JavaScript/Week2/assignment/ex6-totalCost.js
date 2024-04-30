d3b64a4fade020431749550d573f8fa22713bdc6
Alishka <ali.ibrahim.work@gmail.com>
2024-04-30 15:48:08 +0200

    Party done

--- 1-JavaScript/Week2/assignment/ex6-totalCost.js
+++ 1-JavaScript/Week2/assignment/ex6-totalCost.js
@@ -22,21 +22,39 @@ instead!
 -----------------------------------------------------------------------------*/
 const cartForParty = {
   // TODO complete this object
+  wine: 10.50, 
+  cheese: 5.50,
+  juice: 12.50,
+  whiskey: 15.50,
+  vodka: 14.50
 };
 
-function calculateTotalPrice(/* TODO parameter(s) go here */) {
+function calculateTotalPrice(/* TODO parameter(s) go here */cart) {
   // TODO replace this comment with your code
+  let total = 0;
+  for (let item in cart) {
+    if (typeof cart[item] === 'number') {
+      total += cart[item];
+    }
+  }
+  return `Total: €${total.toFixed(2)}`; 
+
+  
 }
 
 // ! Test functions (plain vanilla JavaScript)
 function test1() {
   console.log('\nTest 1: calculateTotalPrice should take one parameter');
   // TODO replace this comment with your code
+  const result = calculateTotalPrice({ apples: 2.99, oranges: 1.49 });
+  console.log(result);   // Output: Total: €4.48
 }
 
 function test2() {
   console.log('\nTest 2: return correct output when passed cartForParty');
   // TODO replace this comment with your code
+  const result = calculateTotalPrice(cartForParty);
+  console.log(result);  // Output: Total: €58.50
 }
 
 function test() {
