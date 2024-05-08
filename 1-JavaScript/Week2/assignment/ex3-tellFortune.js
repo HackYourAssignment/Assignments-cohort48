320e66182cb5a380fd65b6dfd3614a3ed40e74d7
Moayad Mohammed <144966859+TheYodash@users.noreply.github.com>
2024-05-08 14:58:01 +0200

    implemented comments

--- 1-JavaScript/Week2/assignment/ex3-tellFortune.js
+++ 1-JavaScript/Week2/assignment/ex3-tellFortune.js
@@ -31,17 +31,18 @@ randomly select array elements four times inside the `tellFortune` function
 body, this code is now written once only in a separated function.
 -----------------------------------------------------------------------------*/
 
-function selectRandomly(Array) {
-  const x = Math.floor(Math.random() * Array.length);
-  return Array[x];
+function selectRandomly(array) {
+  const x = Math.floor(Math.random() * array.length);
+  return array[x];
 }
 
 function tellFortune(numKids, partnerNames, locations, jobTitles) {
-  const result = `You will be a ${selectRandomly(
-    jobTitles
-  )}, in ${selectRandomly(locations)}, married to ${selectRandomly(
-    partnerNames
-  )} with ${selectRandomly(numKids)} kids`;
+   const numberOfKids = selectRandomly(numKids);
+   const nameOfPartner = selectRandomly(partnerNames);
+   const nameOfLocation = selectRandomly(locations);
+   const nameOfJobTitle = selectRandomly(jobTitles);
+   
+   const result = `You will be a ${nameOfJobTitle}, in ${nameOfLocation}, married to ${partnerNames} with ${numberOfKids} kids`;
 
   return result;
 }
