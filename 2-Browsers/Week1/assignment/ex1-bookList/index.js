680f2b36e93aaf60a37f8088fd3733c59b462444
Alishka <ali.ibrahim.work@gmail.com>
2024-05-21 21:35:58 +0200

    cleaning comment

--- 2-Browsers/Week1/assignment/ex1-bookList/index.js
+++ 2-Browsers/Week1/assignment/ex1-bookList/index.js
@@ -1,4 +1,3 @@
-//cspell: disable
 /*------------------------------------------------------------------------------
 Full description at: https://github.com/HackYourFuture/Assignments/tree/main/2-Browsers/Week1#exercise-1-the-book-list
 
@@ -10,9 +9,9 @@ element with the book title and author.
 3. Use a `<ul>`  and `<li>` to display the books.
 4. Add an `<img>` to each book that links to a URL of the book cover.
 5. Change the style of the book depending on whether you have read it(green) or not(red).
------------------------------------------------------------------------------*/
-
-
+----------------------------------------------------------------------------*/
+// In this assignment you are not allowed to use .innerHTML. To create HTML elements, use document.createElement().
+// To set the text content of an element, use .textContent. The assignment tests currently do not support .innerText.
 
 
 function createBookList(books) {
