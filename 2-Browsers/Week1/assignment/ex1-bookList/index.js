7428b8f57d1ba05288ad3641da995e2793ee4e0f
Alishka <ali.ibrahim.work@gmail.com>
2024-05-21 21:47:36 +0200

    cleaning comment

--- 2-Browsers/Week1/assignment/ex1-bookList/index.js
+++ 2-Browsers/Week1/assignment/ex1-bookList/index.js
@@ -1,17 +1,3 @@
-/*------------------------------------------------------------------------------
-Full description at: https://github.com/HackYourFuture/Assignments/tree/main/2-Browsers/Week1#exercise-1-the-book-list
-
-I'd like to display my three favorite books inside a nice webpage!
-
-1. Iterate through the array of books.
-2. For each book, create a `<p>`
-element with the book title and author.
-3. Use a `<ul>`  and `<li>` to display the books.
-4. Add an `<img>` to each book that links to a URL of the book cover.
-5. Change the style of the book depending on whether you have read it(green) or not(red).
-----------------------------------------------------------------------------*/
-// In this assignment you are not allowed to use .innerHTML. To create HTML elements, use document.createElement().
-// To set the text content of an element, use .textContent. The assignment tests currently do not support .innerText.
 
 
 function createBookList(books) {
