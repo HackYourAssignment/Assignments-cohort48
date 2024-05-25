945b38e9f711f2e3dc5ec24672982130ebcfa28a
Alishka <ali.ibrahim.work@gmail.com>
2024-05-25 09:04:02 +0200

    adding css and js bookList

--- 2-Browsers/Week1/assignment/ex1-bookList/index.js
+++ 2-Browsers/Week1/assignment/ex1-bookList/index.js
@@ -17,8 +17,28 @@ https://hackyourfuture.github.io/example-pages/Browsers/Week1/1-booklist/
 -----------------------------------------------------------------------------*/
 //cspell: enable
 
+// In this assignment you are not allowed to use .innerHTML. To create HTML elements, use document.createElement().
+// To set the text content of an element, use .textContent. The assignment tests currently do not support .innerText.
+
+
 function createBookList(books) {
-  // TODO your code goes in here, return the ul element
+  const ulElement = document.createElement("ul");
+
+  books.forEach(book =>{
+    const liElement = document.createElement("li");
+    const paragraph = document.createElement("p");
+    paragraph.textContent = `${book.title} By ${book.author} ${book.isbn}`
+    liElement.appendChild(paragraph)
+    liElement.style.backgroundColor = book.alreadyRead ? "green " : "red" ;
+
+    const imgCover = document.createElement("img")
+    imgCover.src = `${book.coverBook}`
+    imgCover.alt = `cover of ${book.title}`
+    liElement.appendChild(imgCover)
+
+    ulElement.appendChild(liElement)
+  });
+  return ulElement
 }
 
 function main() {
@@ -28,18 +48,22 @@ function main() {
       author: 'Don Norman',
       isbn: '978-0465050659',
       alreadyRead: false,
+      coverBook : "./assets/the_design_of_everyday_things.jpg"
+      
     },
     {
       title: 'The Most Human Human',
       author: 'Brian Christian',
       isbn: '978-1617933431',
       alreadyRead: true,
+      coverBook : "./assets/the_most_human_human.jpg"
     },
     {
       title: 'The Pragmatic Programmer',
       author: 'Andrew Hunt',
       isbn: '978-0201616224',
       alreadyRead: true,
+      coverBook : "./assets/the_pragmatic_programmer.jpg"
     },
   ];
 
