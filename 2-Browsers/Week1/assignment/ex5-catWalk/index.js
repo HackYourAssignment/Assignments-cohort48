7f447fce4c28b93549b0484de4cc3c8d4e3b0a0c
Ali <ali.ibrahim.work@gmail.com>
2024-05-30 12:51:12 +0200

    add variables in index.js
     add variables. This approach will make your function more flexible, easier to change

--- 2-Browsers/Week1/assignment/ex5-catWalk/index.js
+++ 2-Browsers/Week1/assignment/ex5-catWalk/index.js
@@ -21,6 +21,9 @@ Full description at: https://github.com/HackYourFuture/Assignments/tree/main/2-B
 
    https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif
 -----------------------------------------------------------------------------*/
+const STEP_SIZE_PX = 10;
+const STEP_TIME_MS = 50;
+const DANCE_TIME_MS = 5000;
 window.onload = () => {
    const cat = document.getElementById('img');
    const walkingCatUrl = 'http://www.anniemation.com/clip_art/images/cat-walk.gif';
