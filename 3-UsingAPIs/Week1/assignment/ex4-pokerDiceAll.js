e7ed1cfb8b78deb5dd7dbc52694991e6a5ceca69
mahtabmardani88 <mahtab.mardani88@gmail.com>
2024-06-11 14:07:12 +0200

    mahtab mardani usingApi week1

--- 3-UsingAPIs/Week1/assignment/ex4-pokerDiceAll.js
+++ 3-UsingAPIs/Week1/assignment/ex4-pokerDiceAll.js
@@ -29,7 +29,8 @@ const rollDie = require('../../helpers/pokerDiceRoller');
 function rollDice() {
   // TODO Refactor this function
   const dice = [1, 2, 3, 4, 5];
-  return rollDie(1);
+  const promises = dice.map(d => rollDie(d));
+  return Promise.all(promises)
 }
 
 function main() {
@@ -43,3 +44,11 @@ if (process.env.NODE_ENV !== 'test') {
   main();
 }
 module.exports = rollDice;
+
+
+/* Explanation:
+When one of the promises is rejected, the other dice keep rolling. This happens
+because the `setTimeout` function is still working. Even if a promise fails, the
+`setTimeout` does not stop. So, the other dice will finish rolling and show their
+results in the console.
+*/
