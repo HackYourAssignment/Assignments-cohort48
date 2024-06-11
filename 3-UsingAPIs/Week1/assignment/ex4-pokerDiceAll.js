ea5eba19741066fe294374746c227a6511ead48a
Ali <ali.ibrahim.work@gmail.com>
2024-06-11 21:19:03 +0200

    ex4-pokerDiceAll
    done

--- 3-UsingAPIs/Week1/assignment/ex4-pokerDiceAll.js
+++ 3-UsingAPIs/Week1/assignment/ex4-pokerDiceAll.js
@@ -27,9 +27,14 @@ exercise file.
 const rollDie = require('../../helpers/pokerDiceRoller');
 
 function rollDice() {
-  // TODO Refactor this function
+  
   const dice = [1, 2, 3, 4, 5];
-  return rollDie(1);
+  const promises = dice.map(rollDie)
+
+  return Promise.all(promises)
+  .then((results) => console.log('Resolved!', results))
+  .catch((error) => console.log('Rejected!', error.message));
+
 }
 
 function main() {
